d1b5c8f2fb91f332bb9b8b55f664c473a632fd18
DIVYANKAR BHARGAV <divyankarbhargav@gmail.com>
2020-11-09 12:15:47 +0530

    Added dropdowns

--- assets/js/main.js
+++ assets/js/main.js
@@ -32,3 +32,22 @@ function colorLink() {
   }
 }
 linkColor.forEach((l) => l.addEventListener("click", colorLink));
+
+/* When the user clicks on the button, toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("shows");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("shows")) {
+        openDropdown.classList.remove("shows");
+      }
+    }
+  }
+};
