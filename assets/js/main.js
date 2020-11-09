7a6e1891e900b4113370c266dc818102a0476bfd
DIVYANKAR BHARGAV <divyankarbhargav@gmail.com>
2020-11-09 09:51:54 +0530

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,54 @@
+/*===== SHOW NAVBAR  =====*/
+const showNavbar = (toggleId, navId, bodyId, headerId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId),
+    bodypd = document.getElementById(bodyId),
+    headerpd = document.getElementById(headerId);
+
+  // Validate that all variables exist
+  if (toggle && nav && bodypd && headerpd) {
+    toggle.addEventListener("click", () => {
+      // show navbar
+      nav.classList.toggle("show");
+      // change icon
+      toggle.classList.toggle("bx-x");
+      // add padding to body
+      bodypd.classList.toggle("body-pd");
+      // add padding to header
+      headerpd.classList.toggle("body-pd");
+    });
+  }
+};
+
+showNavbar("header-toggle", "nav-bar", "body-pd", "header");
+
+/*===== LINK ACTIVE  =====*/
+const linkColor = document.querySelectorAll(".nav__link");
+
+function colorLink() {
+  if (linkColor) {
+    linkColor.forEach((l) => l.classList.remove("active"));
+    this.classList.add("active");
+  }
+}
+linkColor.forEach((l) => l.addEventListener("click", colorLink));
+
+/*========== CLICKABLE DROPDOWN ==============*/
+// When the user clicks on the button, toggle between hiding and showing the dropdown content
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
